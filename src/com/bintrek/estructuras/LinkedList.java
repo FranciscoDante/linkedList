8c35aac0752299654af62309b200976e3125c3eb
eldantek <franciscodante22@hotmail.com>
2017-09-12 00:38:13 -0500

    Pruebas de excepciones

--- src/com/bintrek/estructuras/LinkedList.java
+++ src/com/bintrek/estructuras/LinkedList.java
@@ -18,38 +18,51 @@ public class LinkedList<Item> {
     public void insert(Item newItem) {
         Node oldFirst = first;
         Node newNode = new Node();
-        
+
         newNode.data = newItem;
         newNode.next = oldFirst;
         first = newNode;
         size++;
     }
-    
-    public Item get(){
+
+    public Item get() {
         Item value = first.data;
         first = first.next;
-        
+
         size--;
-               
+
         return value;
     }
-    
-    public Item delete(int numNode){
-        if(numNode <= size){
-            Node previo = first;
-            
-            for(int i=1; i<=size; i++){
-                if(i == numNode){
-                    
+
+    public Item delete(int numNode) {
+        if (numNode > 0 && numNode <= size) {
+            Node current = first;
+            Node prev = null;
+
+            for (int i = 1; i <= size; i++) {
+                if (i == numNode) {
+                    if (prev == null) {
+                        first = first.next;
+                        size--;
+                        return first.data;
+                    } else {
+                        prev.next = current.next;
+                        size--;
+                        return current.data;
+                    }
+                } else {
+                    prev = current;
+                    current = current.next;
                 }
             }
             return null;
-        }else{
+        } else {
             return null;
         }
     }
 
     private class Node {
+
         Item data;
         Node next;
     }
--- src/com/bintrek/estructuras/TestLinkedList.java
+++ src/com/bintrek/estructuras/TestLinkedList.java
@@ -13,15 +13,42 @@ public class TestLinkedList {
     public static void main( String [] arg ){
         LinkedList<String> list = new LinkedList<>();
         
-        list.insert("Hola");
-        list.insert("lista");
-        list.insert("ligada");
+        list.insert("ele 14");
+        list.insert("ele 13");
+        list.insert("ele 12");
+        list.insert("ele 11");
+        list.insert("ele 10");
+        list.insert("ele 9");
+        list.insert("ele 8");
+        list.insert("ele 7");
+        list.insert("ele 6");        
+        list.insert("ele 5");
+        list.insert("ele 4");
+        list.insert("ele 3");
+        list.insert("ele 2");
+        list.insert("ele 1");
         
         System.out.println("Elementos agregados");
         
         int tamanio = list.size;
         
+//        for(int i=1; i<=tamanio; i++){
+//            System.out.println( "Elemento " + i + ":" + list.get() );
+//        }
+        
+//        list.delete(9);
+//        list.delete(1);
+                
+        tamanio = list.size;
+        System.out.println( tamanio );
+        
         for(int i=1; i<=tamanio; i++){
+            System.out.println( "it: "+ i + " - Elemento eliminado:" + list.delete(i) );
+        }
+        
+        System.out.println("===============================");
+        
+         for(int i=1; i<=list.size; i++){
             System.out.println( "Elemento " + i + ":" + list.get() );
         }
         
